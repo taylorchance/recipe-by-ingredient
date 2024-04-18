499e4902695eea9d3ce2b40202e5e786a5ffaca1
Taylor Chance <taylorchance@gmail.com>
2024-04-18 11:43:12 -0400

    dropdown working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,11 +37,58 @@ $: q = query(collection(firestore, 'recipes'),
 export let data
 console.log('data', data)
 
-// const doFunc = async () => {
-// 	const temp = await getRecipes()
-// 	console.log('temp', temp)
-// }
-// doFunc()
+const submitValue = () => {
+	if (search) {
+		console.log(`${search} is submitted!`);
+		// setTimeout(clearInput, 1000);
+	} else {
+		alert("You didn't type anything.")
+	}
+}
+
+let filteredIngredients = []
+
+const filterRecipes = () => {
+	console.log('filterRecipes')
+	let storageArr = []
+	if (search) {
+		data.ingredients.forEach(ingredient => {
+			// if (ingredient.toLowerCase().startsWith(search.toLowerCase())) {
+			if (ingredient.toLowerCase().includes(search.toLowerCase())) {
+				storageArr = [...storageArr, makeMatchBold(ingredient)]
+			}
+		})
+	}
+	filteredIngredients = storageArr
+}
+
+const makeMatchBold = (str) => {
+	// replace part of (country name === inputValue) with strong tags
+	let matched = str.substring(0, search.length)
+	let makeBold = `<strong>${matched}</strong>`
+	let boldedMatch = str.replace(matched, makeBold)
+	return boldedMatch
+}
+
+let hiLiteIndex = null
+
+const navigateList = (e) => {
+	if (e.key === "ArrowDown" && hiLiteIndex <= filteredIngredients.length-1) {
+		hiLiteIndex === null ? hiLiteIndex = 0 : hiLiteIndex += 1
+	} else if (e.key === "ArrowUp" && hiLiteIndex !== null) {
+		hiLiteIndex === 0 ? hiLiteIndex = filteredIngredients.length-1 : hiLiteIndex -= 1
+	} else if (e.key === "Enter") {
+		setInputVal(filteredIngredients[hiLiteIndex])
+	} else {
+		return
+	}
+}
+
+const setInputVal = (ingredient) => {
+	search = ingredient.replace(/<(.)*?>/g, "")
+	filteredIngredients = []
+	hiLiteIndex = null
+}	
 </script>
 
 <svelte:head>
@@ -49,16 +96,49 @@ console.log('data', data)
 	<meta name="description" content="What to make with" />
 </svelte:head>
 
+<svelte:window on:keydown={navigateList} />
+
 <section class="section">
 	<div class="container">
 		<h1 class="title">
 			Lookup an ingredient to find some recipes
 		</h1>
 
-		<div class="field">
-			<input class="input" bind:value={search} />
-			<button class="button mt-3" on:click={handleSubmit}>Search!</button>
+		<div class="field has-addons">
+			<div class="control">
+				<div class={`dropdown ${filteredIngredients.length > 0 ? "is-active" : ""}`}>
+					<div class="dropdown-trigger">
+						<input
+							class="input"
+							type="text"
+							placeholder="Search Recipes"
+							bind:value={search} 
+							on:input={filterRecipes}
+						/>
+					</div>
+					<div class="dropdown-menu pt-0">
+						<div class="dropdown-content pt-0">
+							{#each filteredIngredients as ingredient, index}
+								<a
+									class={`dropdown-item ${index === hiLiteIndex ? "is-active" : ""}`}
+									href="/"
+									on:click={setInputVal(ingredient)}
+								>
+									{@html ingredient}
+								</a>
+								<!-- <Country itemLabel={recipe} highlighted={i === hiLiteIndex} on:click={() => setInputVal(recipe)} /> -->
+							{/each}
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="control">
+				<button class="button is-info">
+					Search
+				</button>
+			</div>
 		</div>
+
 
 		<!-- {#each recipes as recipe}
 			<p>{recipe.name}</p>
